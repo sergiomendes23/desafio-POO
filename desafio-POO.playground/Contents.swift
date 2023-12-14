75a18eaf0d716170b2888f2420747eb007662ccd
Sergio Mendes <sergiomendes@MacBook-Air-de-Sergio.local>
2023-12-14 16:29:30 -0300

    create class

--- desafio-POO.playground/Contents.swift
+++ desafio-POO.playground/Contents.swift
@@ -1,4 +1,28 @@
 import UIKit
 
-var greeting = "Hello, playground"
-print(greeting)
+class Band {
+    var vocal: String
+    var bass: String
+    var guitar: String
+    var drums: String
+    
+    init(vocal: String, bass: String, guitar: String, drums: String){
+        self.vocal = vocal
+        self.bass = bass
+        self.guitar = guitar
+        self.drums = drums
+    }
+}
+
+class MembersOfBand {
+    var name: String
+    var age: Int
+    
+    init(name: String, age: Int){
+        self.name = name
+        self.age = age
+    }
+}
+
+let drumPlayer = MembersOfBand(name: "Rony", age: 29)
+print("O baterista é o \(drumPlayer.name) e ele tem \(drumPlayer.age) anos")
