a7bbd6641d1b4fc6be705d73829417169a23e62c
Sergio Mendes <sergiomendes@MacBook-Air-de-Sergio.local>
2023-12-18 14:04:40 -0300

    herança e polimorfismo Done

---
+++ desafio-POO.playground/Pages/POO.xcplaygroundpage/Contents.swift
@@ -0,0 +1,63 @@
+import UIKit
+
+class Band {
+    var vocal: String
+    var bass: String
+    var guitar: String
+    var drums: String
+    
+    init(vocal: String, bass: String, guitar: String, drums: String){
+        self.vocal = vocal
+        self.bass = bass
+        self.guitar = guitar
+        self.drums = drums
+    }
+}
+
+class MembersOfBand {
+    var name: String
+    var age: Int
+    
+    init(name: String, age: Int){
+        self.name = name
+        self.age = age
+    }
+    
+    func play (music: String) {
+        print("Tipo musical: \(music)")
+    }
+}
+var players = Band(vocal: "Salê", bass: "Caio Che", guitar: "Gods", drums: "Rony")
+let rony = MembersOfBand(name: "Rony", age: 30)
+let caio = MembersOfBand(name: "Caio Che", age: 31)
+let gods = MembersOfBand(name: "Gods", age: 30)
+let sale = MembersOfBand(name: "Salê", age: 31)
+print(
+    "vocal: \(players.vocal), \(sale.age) anos.",
+    "guitarrista: \(players.guitar), \(gods.age) anos.",
+    "baixo: \(players.bass), \(caio.age) anos.",
+    "bateirista: \(players.drums), \(rony.age) anos."
+)
+print(sale.play(music: "Rock"))
+
+class CountInstrument {
+    let guitar1: Int!
+    let guitar2: Int!
+    
+    private var result: Int!
+    
+    init(guitar1: Int, guitar2: Int){
+        self.guitar1 = guitar1
+        self.guitar2 = guitar2
+    }
+    
+    func count () {
+        result = guitar1 + guitar2
+    }
+    
+    func resultCount () {
+        print("Se \(players.guitar) não tem 2, ele tem \(result ?? 0) guitarras.")
+    }
+}
+let godsGuitars = CountInstrument(guitar1: 1, guitar2: 1)
+godsGuitars.resultCount()
